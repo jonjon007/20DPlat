150a2a0f9ca4c18d549d955ea5c4d2d6b5003635
jonjon007 <jomartin@udel.edu>
2019-10-16 13:02:31 -0400

    Added decent jumping capability

--- Assets/_SCRIPTS_/PlayerController.cs
+++ Assets/_SCRIPTS_/PlayerController.cs
@@ -7,6 +7,9 @@ public class PlayerController : MonoBehaviour
     Rigidbody rb;
     int walkSpeed = 7;
     int jumpHeight = 800;
+    public bool inAir = true;
+
+    Ray groundRay; //For checking the ground below
     void Awake(){
         rb = GetComponent<Rigidbody>();
         if(rb == null)
@@ -22,6 +25,7 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        drawRays();
         Vector3 newVel = rb.velocity;
         if(Input.GetKey("left")){
             Vector3 temp = -transform.right*walkSpeed;
@@ -37,7 +41,26 @@ public class PlayerController : MonoBehaviour
         rb.velocity = newVel;
 
         //Jump
-        if(Input.GetKeyDown("up"))
+        if(Input.GetKeyDown("up") && !inAir){
+            inAir = true;
             rb.AddForce(transform.up*jumpHeight);
+        }
+    }
+
+    void drawRays(){
+        groundRay = new Ray (transform.position, -transform.TransformDirection(transform.up)*1.5f);
+        Debug.DrawRay (transform.position, -transform.TransformDirection(transform.up)*1.5f, Color.cyan);
+    }
+
+    //Unity's built-in
+    void OnCollisionEnter(Collision other){
+        if(other.gameObject.CompareTag("Ground"))
+            inAir = false;
+    }
+
+    //Unity's built-in
+    void OnCollisionExit(Collision other){
+        if(other.gameObject.CompareTag("Ground"))
+            inAir = true;
     }
 }
