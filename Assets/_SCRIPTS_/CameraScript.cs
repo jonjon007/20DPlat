86be3a59c7b4260f124811bdb8c6e57d24c68981
jonjon007 <jomartin@udel.edu>
2019-10-16 12:49:47 -0400

    First commit. Yarg

---
+++ Assets/_SCRIPTS_/CameraScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+    Transform player; //Player to follow
+    // Start is calle   d before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        if(player == null)
+            Debug.LogError("Camera couldn't find the player!");
+        else
+            Debug.Log("Yarg from Camera");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(player.position.x, player.position.y, this.transform.position.z);
+    }
+}
---
+++ Assets/_SCRIPTS_/PlayerController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    Rigidbody rb;
+    int walkSpeed = 7;
+    int jumpHeight = 800;
+    void Awake(){
+        rb = GetComponent<Rigidbody>();
+        if(rb == null)
+            Debug.LogError("No Rigidbody found in player!");
+    }
+
+    // Start is called before the first frame update    
+    void Start()
+    {
+        print("Yarg");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 newVel = rb.velocity;
+        if(Input.GetKey("left")){
+            Vector3 temp = -transform.right*walkSpeed;
+            temp.y = newVel.y;
+            newVel = temp;
+        }
+        if(Input.GetKey("right")){
+            Vector3 temp = transform.right*walkSpeed;
+            temp.y = newVel.y;
+            newVel = temp;
+        }
+       //     rb.MovePosition(transform.position + transform.right*.2f);
+        rb.velocity = newVel;
+
+        //Jump
+        if(Input.GetKeyDown("up"))
+            rb.AddForce(transform.up*jumpHeight);
+    }
+}
